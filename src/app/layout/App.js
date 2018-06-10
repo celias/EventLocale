c79b469b2f707480042adc12d93b9d974ffe4605
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-10 11:57:39 -0500

    assets

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import NavBar from '../../features/nav/navBar/NavBar';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Event Locale</h2>
+        <EventDashboard />
+        <NavBar />
       </div>
     );
   }
