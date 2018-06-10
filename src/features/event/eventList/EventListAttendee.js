97027749639a1aa0c0fe5704fb5f3d32f31ddbcc
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-10 14:50:04 -0500

    EventDashboard, EventList

--- src/features/event/eventList/EventListAttendee.js
+++ src/features/event/eventList/EventListAttendee.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
-import { List, Image } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { List, Image } from 'semantic-ui-react';
+
 class EventListAttendee extends Component {
   render() {
+    const {attendee} = this.props;
     return (
       <List.Item>
-        <Image as='a' size='mini' rounded src="http://thecatapi.com/api/images/get?format=src&results_per_page=20" />
+        <Image as='a' size='mini' rounded src={attendee.photoURL} />
       </List.Item>
     )
   }
