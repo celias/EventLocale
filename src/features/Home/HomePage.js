b9b914f8e0dff0e6442f5f52b14f4ac4b2b8dcd8
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-10 23:46:07 -0500

    landing start

--- src/features/Home/HomePage.js
+++ src/features/Home/HomePage.js
@@ -2,9 +2,42 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>HomePage</h1>
-    </div>
+      <div>
+            <div className="ui inverted vertical masthead center aligned segment">
+              <div className="ui text container">
+                <h1 className="ui inverted stackable header">
+                  <img
+                    className="ui image massive"
+                    src="/assets/logo.png"
+                    alt="logo"
+                  />
+                  <div className="content">Event Locale</div>
+                </h1>
+                <h2>Let's do stuff!</h2>
+                <div className="ui huge white inverted button">
+                  Get Started
+                  <i className="right arrow icon" />
+                </div>
+              </div>
+            </div>
+            <div style={{ textAlign: 'center' }}>
+              Icons made by{' '}
+              <a href="http://www.freepik.com" title="Freepik">
+                Freepik
+              </a>{' '}
+              from{' '}
+              <a href="https://www.flaticon.com/" title="Flaticon">
+                www.flaticon.com
+              </a>{' '}
+              is licensed by{' '}
+              <a
+                href="http://creativecommons.org/licenses/by/3.0/"
+                title="Creative Commons BY 3.0"
+              >
+                CC 3.0 BY
+              </a>
+            </div>
+        </div>
   )
 }
 
