b5f9e72289ea9095f803282ae9c565c3bc058e94
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-13 14:12:21 -0500

    conditional rendering in the navbar and signedInMenu

--- src/features/nav/navBar/NavBar.js
+++ src/features/nav/navBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 
@@ -19,6 +19,8 @@ class NavBar extends Component {
 		this.setState({
 			authenticated: false
 		});
+		// This works because of Higher order components (withRouter)
+		this.props.history.push('/');
 	};
 
 	render() {
@@ -32,11 +34,19 @@ class NavBar extends Component {
 							Event Locale
 						</Menu.Item>
 						<Menu.Item as={NavLink} to="/events" name="Events" />
-						<Menu.Item as={NavLink} to="/people" name="People" />
+						{authenticated && <Menu.Item as={NavLink} to="/people" name="People" />}
+						{authenticated && (
+							<Menu.Item>
+								<Button
+									as={Link}
+									to="/createEvent"
+									floated="right"
+									color="teal"
+									content="Create Event"
+								/>
+							</Menu.Item>
+						)}
 
-						<Menu.Item>
-							<Button as={Link} to="/createEvent" floated="right" color="teal" content="Create Event" />
-						</Menu.Item>
 						{authenticated ? (
 							<SignedInMenu signOut={this.handleSignOut} />
 						) : (
@@ -48,4 +58,4 @@ class NavBar extends Component {
 		);
 	}
 }
-export default NavBar;
+export default withRouter(NavBar);
