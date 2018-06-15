ac3f60b02b49138b94801c8290e72fb313c05f81
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-15 15:44:03 -0500

    event details

--- src/features/events/EventDetailed/EventDetailedPage.js
+++ src/features/events/EventDetailed/EventDetailedPage.js
@@ -5,16 +5,43 @@ import EventDetailedChat from './EventDetailedChat';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedSidebar from './EventDetailedSidebar';
 
+const event = {
+  
+    id: '1',
+    title: 'Trip to Fiesta Market',
+    date: '2018-03-27',
+    category: 'culture',
+    description:
+      'AvoCATos and Catnip!',
+    city: '?, TX',
+    venue: "Fiesta Market, ?",
+    hostedBy: 'Senor Bob',
+    hostPhotoURL: 'http://thecatapi.com/api/images/get?format=src&results_per_page=20',
+    attendees: [
+      {
+        id: 'a',
+        name: 'Senor Bob',
+        photoURL: 'http://thecatapi.com/api/images/get?format=src&results_per_page=20'
+      },
+      {
+        id: 'b',
+        name: 'Tom Cat',
+        photoURL: 'http://thecatapi.com/api/images/get?format=src&results_per_page=22'
+      }
+    ]
+  }
+
+
 const EventDetailedPage = () => {
 	return (
 		<Grid>
 			<Grid.Column width={10}>
-				<EventDetailedHeader />
-				<EventDetailedInfo />
+				<EventDetailedHeader event={event} />
+				<EventDetailedInfo event={event} />
 				<EventDetailedChat />
 			</Grid.Column>
 			<Grid.Column width={6}>
-				<EventDetailedSidebar />
+				<EventDetailedSidebar attendees={event.attendees}/>
 			</Grid.Column>
 		</Grid>
 	);
