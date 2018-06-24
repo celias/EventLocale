dea84e2b4577d54cd728de894f73e74d2bcecc73
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-06-24 14:45:36 -0500

    react router scrolltotop

--- src/features/events/EventDetailed/EventDetailedPage.js
+++ src/features/events/EventDetailed/EventDetailedPage.js
@@ -1,38 +1,27 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux'
 import EventDetailedHeader from './EventDetailedHeader';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedSidebar from './EventDetailedSidebar';
 
-const event = {
-  
-    id: '1',
-    title: 'Trip to Fiesta Market',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'AvoCATos and Catnip!',
-    city: '?, TX',
-    venue: "Fiesta Market, ?",
-    hostedBy: 'Senor Bob',
-    hostPhotoURL: 'http://thecatapi.com/api/images/get?format=src&results_per_page=20',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Senor Bob',
-        photoURL: 'http://thecatapi.com/api/images/get?format=src&results_per_page=20'
-      },
-      {
-        id: 'b',
-        name: 'Tom Cat',
-        photoURL: 'http://thecatapi.com/api/images/get?format=src&results_per_page=22'
-      }
-    ]
+const mapState = (state, ownProps) => {
+  const eventId = ownProps.match.params.id;
+
+  let event = {};
+
+  if(eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0]
   }
 
+  return {
+    event
+  }
+}
 
-const EventDetailedPage = () => {
+
+const EventDetailedPage = ({event}) => {
 	return (
 		<Grid>
 			<Grid.Column width={10}>
@@ -47,4 +36,4 @@ const EventDetailedPage = () => {
 	);
 };
 
-export default EventDetailedPage;
+export default connect(mapState)(EventDetailedPage);
